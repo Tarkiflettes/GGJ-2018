eebc4f5ea97dfaed8520477334070c6402098da0
Ludo35000 <ludovicrabu@gmail.com>
2018-01-26 23:26:13 +0100

    First commit

---
+++ Assets/Scripts/Interactives/Switch.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Interactives;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : Trigger {
+    protected override void Action()
+    {
+        throw new System.NotImplementedException();
+        Debug.Log("Action");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
