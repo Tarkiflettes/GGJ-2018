59b1e5e9b763a078db8511eabff0bfa1e553ca9b
Lionel Jouin <lionel.jouin@outlook.com>
2018-01-26 21:32:36 +0100

    trigger/triggered

---
+++ Assets/Scripts/Interactives/Trigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public List<Triggered> TriggeredList;
+    
+    void Start()
+    {
+        TriggeredList = new List<Triggered>();
+    }
+
+    void Action()
+    {
+        foreach (var t in TriggeredList)
+        {
+            t.Action();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Interactives/Triggered.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Triggered : MonoBehaviour
+{
+
+    public abstract void Action();
+
+}
