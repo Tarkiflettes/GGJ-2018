038ab18e83ac272b3b38b65557d6f34ce9faeacc
Ludo35000 <ludovicrabu@gmail.com>
2018-01-27 00:24:39 +0100

    Button script

--- Assets/Scripts/Interactives/Switch.cs
+++ Assets/Scripts/Interactives/Switch.cs
@@ -1,34 +1,30 @@
 ﻿using Assets.Scripts.Interactives;
-<<<<<<< HEAD
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch : Trigger {
-    protected override void Action()
+public class Switch : Trigger
+{
+
+    private IEnumerator ContinuousRotation(Vector3 vector)
     {
-        throw new System.NotImplementedException();
-        Debug.Log("Action");
+        for (var i = 0; i < 45; i++)
+        {
+            transform.Rotate(vector, 1);
+            yield return new WaitForSeconds(0.01f);
+        }
     }
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-=======
-using UnityEngine;
-
-public class Switch : Trigger {
 
     protected override void Action()
     {
-        Debug.Log("koukou");
+        Vector3 vector = Vector3.forward;
+
+        if (transform.rotation == Quaternion.Euler(0, 0, 45))
+        {
+            vector = Vector3.back;
+        }
+
+        var continuousRotation = ContinuousRotation(vector);
+        StartCoroutine(continuousRotation);
     }
 
->>>>>>> Lionel
 }
