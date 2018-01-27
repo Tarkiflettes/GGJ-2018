5689ea2270def3709409b4d9b2af9470e9383b85
Ludo35000 <ludovicrabu@gmail.com>
2018-01-27 11:34:54 +0100

    OpenBook

---
+++ Assets/Scripts/Interactives/OpenBook.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBook : Trigger
+{
+    protected override void Action()
+    {
+        foreach (Transform child in transform.parent)
+        {
+            Debug.Log(child.name + " : " +  child.eulerAngles);
+            child.eulerAngles = new Vector3(child.eulerAngles.x+45 , child.eulerAngles.y+90, child.eulerAngles.z+90);
+        }
+    }
+}
