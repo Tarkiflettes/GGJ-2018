d0e810236c72f1e8ece34919f973f4c2925418f4
Rhohen <nico.giro72@gmail.com>
2018-01-27 17:46:09 +0100

    Opening of the dorr edited

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Chest : Triggered {
 
     public Rotator Rotator;
-    private bool opened;
+    private bool _opened;
 
     [ContextMenu("Open")]
     public override void Action()
     {
-        if(!opened)
+        if(!_opened)
         {
             Rotator.Open(Vector3.left, 50);
         } else
         {
             Rotator.Close();
         }
-        opened = !opened;
+        _opened = !_opened;
     }
 }
