5617cef2be30e9b43460efafef371ff52b55a1f3
Lionel Jouin <lionel.jouin@outlook.com>
2018-01-27 17:24:29 +0100

    menu

---
+++ Assets/Scripts/Interactives/Drawer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : Triggered
+{
+
+    public bool Closed = true;
+
+    private int _closed = 1;
+    private bool _busy = false;
+
+    public override void Action()
+    {
+        if (_busy) return;
+
+        _busy = true;
+        var openClose = OpenClose();
+        StartCoroutine(openClose);
+    }
+
+    private IEnumerator OpenClose()
+    {
+        _closed = 1;
+        if (Closed)
+            _closed = -1;
+
+        for (var i = 0; i < 90; i++)
+        {
+            transform.Rotate(Vector3.forward, _closed);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        Closed = !Closed;
+        _busy = false;
+    }
+}
