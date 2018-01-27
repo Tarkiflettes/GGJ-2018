06f6cce322b59dd339a0a4c4050e67f24d82e537
Ludo35000 <ludovicrabu@gmail.com>
2018-01-27 13:20:47 +0100

    Fin open book

--- Assets/Scripts/Interactives/OpenBook.cs
+++ Assets/Scripts/Interactives/OpenBook.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class OpenBook : Trigger
 {
+    private bool open = false;
+    private bool _busy = false;
+    public float speed = 0.01f;
+
+    private IEnumerator ContinuousRotation()
+    {
+        
+        for (var i = 0; i < (int)(45/speed); i++)
+        {
+            foreach (Transform child in transform.parent)
+            {
+                if (!open)
+                    child.Rotate(Vector3.down, speed);
+                else
+                    child.Rotate(Vector3.up, speed);
+            }
+                yield return new WaitForSeconds(0.1f);
+        }
+        _busy = false;
+        open = !open;
+    }
+
     protected override void Action()
     {
-        foreach (Transform child in transform.parent)
+        if (!_busy)
         {
-            Debug.Log(child.name + " : " +  child.eulerAngles);
-            child.eulerAngles = new Vector3(child.eulerAngles.x+45 , child.eulerAngles.y+90, child.eulerAngles.z+90);
+            _busy = true;
+            var rotate = ContinuousRotation();
+            StartCoroutine(rotate);
         }
     }
 }
