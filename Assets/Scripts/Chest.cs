126aaf5a3671fa2111314903d170e68a8d9c77b6
FlorianPgn <florianpgn@gmail.com>
2018-01-27 17:24:21 +0100

    Open system

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Triggered {
+
+    public Rotator Rotator;
+    private bool opened;
+
+    [ContextMenu("Open")]
+    public override void Action()
+    {
+        if(!opened)
+        {
+            Rotator.Open(Vector3.left, 50);
+        } else
+        {
+            Rotator.Close();
+        }
+        opened = !opened;
+    }
+}
