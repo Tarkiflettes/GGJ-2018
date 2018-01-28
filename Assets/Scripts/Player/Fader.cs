b1493fca2268c756edc793467906afc6d15c60e8
FlorianPgn <florianpgn@gmail.com>
2018-01-28 13:53:20 +0100

    Almost ended last R1P4

--- Assets/Scripts/Player/Fader.cs
+++ Assets/Scripts/Player/Fader.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Fader : MonoBehaviour {
 
-    public float Speed;
+    public float Speed = 5f;
     private float _fadeIntensity;
     private Color _color;
     private bool _fadeIn;
