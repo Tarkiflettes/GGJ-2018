ea715df0f3562edd6e77b7f76e0f5c10e771c11b
FlorianPgn <florianpgn@gmail.com>
2018-01-28 15:28:36 +0100

    Fin R4P1

---
+++ Assets/Scripts/Interactives/RuneTable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneTable : RayReceiver {
+
+    public GameObject[] Runes;
+    public Pickable[] RunesPickables;
+    public Animator TableCenterAnimator;
+    public Player Player;
+
+    private int _nbRunePlaced;
+
+    protected override void OnRayEnter()
+    {
+
+    }
+
+    protected override void OnRayExit()
+    {
+    }
+
+    protected override void OnRaySelect()
+    {
+        foreach (Pickable rune in RunesPickables)
+        {
+            if(Player.Inventory.Contains(rune))
+            {
+                Player.Inventory.RemovePickable(rune); 
+                Runes[_nbRunePlaced++].SetActive(true);
+                if(_nbRunePlaced == Runes.Length)
+                {
+                    new WaitForSeconds(1f);
+                    TableCenterAnimator.SetTrigger("Open");
+                }
+            }
+
+        }
+    }
+}
